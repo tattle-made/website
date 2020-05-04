d0b650b3c893d9e0d575b724444d57b5773c14d4
denny <denny.george90@gmail.com>
2020-05-04 14:43:50 +0530

    (feat) wip adding product dropdown

--- src/components/atomic/SimpleHeader.js
+++ src/components/atomic/SimpleHeader.js
@@ -1,51 +1,59 @@
-import React from 'react'
-import { Box, Heading, Button} from 'grommet';
-import AppLogo from './AppLogo';
-import styled from 'styled-components';
-import { PlainLink as Link, PlainExternalLink as ExternalLink } from './TattleLinks';
+import React from "react"
+import { Box, Heading, Button } from "grommet"
+import AppLogo from "./AppLogo"
+import styled from "styled-components"
+import {
+  PlainLink as Link,
+  PlainExternalLink as ExternalLink,
+} from "./TattleLinks"
 
 const ThemedLink = styled(Link)`
-    text-decoration: none;
+  text-decoration: none;
+  color: inherit;
+  :visited {
     color: inherit;
-    :visited {
-        color: inherit;
-    }
+  }
 `
 
-
-
 /**
-* @author
-* @function SimpleHeader
-**/
+ * @author
+ * @function SimpleHeader
+ **/
 
-const SimpleHeader = ({label, target, primaryNav}) => (
-    <Box direction={'row'} wrap={true}>
-        <AppLogo 
-            name={label}
-            target={target}
-        />
+const SimpleHeader = ({ label, target, primaryNav }) => (
+  <Box direction={"row"} wrap={true}>
+    <AppLogo name={label} target={target} />
 
-        <Box margin={{left: 'large'}} pad={'small'} direction={'row'} align={'center'} gap={'medium'}>
-            {   
-                primaryNav.options.map((option) => (
-                    option.type == 'external' ?
-                        <ExternalLink key={option.id} href={option.target} target={'_blank'} > 
-                            <Button plain={true}>
-                                <Heading margin={'none'} level={3}> {option.label} </Heading>
-                            </Button>
-                        </ExternalLink>
-                    :
-                        <Link key={option.id} to={option.target} > 
-                            <Button plain={true}>
-                                <Heading margin={'none'} level={3}> {option.label} </Heading>
-                            </Button>
-                        </Link>
-                ))
-            }
-
-        </Box>
+    <Box
+      margin={{ left: "large" }}
+      pad={"small"}
+      direction={"row"}
+      align={"center"}
+      gap={"medium"}
+    >
+      {primaryNav.options.map(option =>
+        option.type == "external" ? (
+          <ExternalLink key={option.id} href={option.target} target={"_blank"}>
+            <Button plain={true}>
+              <Heading margin={"none"} level={3}>
+                {" "}
+                {option.label}{" "}
+              </Heading>
+            </Button>
+          </ExternalLink>
+        ) : (
+          <Link key={option.id} to={option.target}>
+            <Button plain={true}>
+              <Heading margin={"none"} level={3}>
+                {" "}
+                {option.label}{" "}
+              </Heading>
+            </Button>
+          </Link>
+        )
+      )}
     </Box>
+  </Box>
 )
 
 export default SimpleHeader
--- src/config/options.js
+++ src/config/options.js
@@ -1,18 +1,42 @@
 export const footerItems = {
-    primary : [
-        { id : 1, label : 'Github', target: 'https://github.com/tattle-made', type:'external'},
-        { id : 2, label : 'Contact', target: '/contact', type:'internal'},
-        { id : 3, label : 'Privacy Policy', target: '/privacy-policy', type:'internal'},
-    ],
+  primary: [
+    {
+      id: 1,
+      label: "Github",
+      target: "https://github.com/tattle-made",
+      type: "external",
+    },
+    { id: 2, label: "Contact", target: "/contact", type: "internal" },
+    {
+      id: 3,
+      label: "Privacy Policy",
+      target: "/privacy-policy",
+      type: "internal",
+    },
+  ],
 }
 
-
 export const primaryNav = {
-    options : [
-        {id: 0, label: 'Khoj', target:'https://services.tattle.co.in/khoj', type:'external'},
-        {id: 1, label: 'Archive', target:'http://shell.tattle.co.in', type:'external'},
-        {id: 2, label: 'Jod Bot', target:'https://t.me/TattleJodBot', type:'external'},
-        {id: 3, label: 'FAQ', target:'/faq', type:'internal'},
-        {id: 4, label: 'Join Us', target:'/join-us', type:'internal'},
-    ]
+  options: [
+    {
+      id: 0,
+      label: "Khoj",
+      target: "https://services.tattle.co.in/khoj",
+      type: "external",
+    },
+    {
+      id: 1,
+      label: "Archive",
+      target: "http://shell.tattle.co.in",
+      type: "external",
+    },
+    {
+      id: 2,
+      label: "Jod Bot",
+      target: "https://t.me/TattleJodBot",
+      type: "external",
+    },
+    { id: 3, label: "FAQ", target: "/faq", type: "internal" },
+    { id: 4, label: "Join Us", target: "/join-us", type: "internal" },
+  ],
 }
