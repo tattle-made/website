c029de3bc9d4a957ab669bc9671deab54c38adac
suraj <surajsharma@pm.me>
2020-06-08 19:44:20 +0530

    responsive contributor grid

--- src/pages/contributors.js
+++ src/pages/contributors.js
@@ -1,18 +1,8 @@
 import React from "react"
-import {
-  Box,
-  Heading,
-  Text,
-  Paragraph,
-  Grid,
-  ResponsiveContext,
-  Avatar,
-  Icons,
-  Grommet,
-} from "grommet"
-
 import { primaryNav, footerItems } from "../config/options"
 import AppShell from "../components/atomic/AppShell"
+import ResponsiveContributorGrid from "../components/atomic/ResponsiveContributorGrid"
+import { Text } from "grommet"
 
 const columns = {
   small: ["auto"],
@@ -21,8 +11,6 @@ const columns = {
 
 // If the size is small, we have 3 rows
 // If the size is medium, we have 2 rows
-// If the size is large or xlarge, we have 1 row
-
 const rows = {
   small: ["auto", "auto", "auto", "auto"],
   medium: ["auto", "auto"],
@@ -44,133 +32,6 @@ const fixedGridAreas = {
   ],
 }
 
-const Responsive = ({
-  children,
-  overrideColumns,
-  overrideRows,
-  areas,
-  ...props
-}) => (
-  <ResponsiveContext.Consumer>
-    {size => {
-      // Take into consideration if not array is sent but a simple string
-      let columnsVal = columns
-      if (columns) {
-        if (columns[size]) {
-          columnsVal = columns[size]
-        }
-      }
-
-      let rowsVal = rows
-      if (rows) {
-        if (rows[size]) {
-          rowsVal = rows[size]
-        }
-      }
-
-      // Also if areas is a simple array not an object of arrays for
-      // different sizes
-      let areasVal = areas
-      if (areas && !Array.isArray(areas)) areasVal = areas[size]
-
-      return (
-        <Grid
-          {...props}
-          areas={!areasVal ? undefined : areasVal}
-          rows={!rowsVal ? size : rowsVal}
-          columns={!columnsVal ? size : columnsVal}
-        >
-          {children}
-        </Grid>
-      )
-    }}
-  </ResponsiveContext.Consumer>
-)
-
-const ResponsiveGrid = props => (
-  <Grommet>
-    <Box>
-      <Responsive
-        rows={rows}
-        columns={columns}
-        gap="small"
-        areas={fixedGridAreas}
-        margin="medium"
-      >
-        <Text size={"xlarge"}>{props.title}</Text>
-        <Box
-          gridArea="con1"
-          responsive="true"
-          direction="column"
-          gap="small"
-          pad="small"
-        >
-          <Avatar
-            size="xlarge"
-            src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80"
-          />
-          <Text weight="bold" size="medium" color="red">
-            Denny George
-          </Text>
-          <Text weight="bold" size="medium" color="dark-3">
-            Interaction design and tech lead
-          </Text>
-          <Text size="medium" color="dark-3">
-            Past: Director of Mobile Products on Micragram, and WorkIt at
-            Quadrant2; <br /> New York ITP, Tisch School of Arts, NYU
-          </Text>
-        </Box>
-        <Box
-          gridArea="con2"
-          responsive="true"
-          direction="column"
-          gap="small"
-          pad="small"
-        >
-          <Avatar
-            size="xlarge"
-            src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80"
-          />
-          <Text weight="bold" size="medium" color="red">
-            Tarunima Prabhakar
-          </Text>
-          <Text weight="bold" size="medium" color="dark-3">
-            Research and Project Lead
-          </Text>
-          <Text size="medium" color="dark-3">
-            Past: Research Fellow, CLTC, UC, Berkeley;
-            <br />
-            Facebook Policy Research
-          </Text>
-        </Box>
-        <Box
-          gridArea="con3"
-          responsive="true"
-          direction="column"
-          gap="small"
-          pad="small"
-        >
-          <Avatar
-            size="xlarge"
-            src="//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80"
-          />
-          <Text weight="bold" size="medium" color="red">
-            Kruttika Nadig
-          </Text>
-          <Text weight="bold" size="medium" color="dark-3">
-            Data Scientist
-          </Text>
-          <Text size="medium" color="dark-3">
-            Past: Marketing Manager, Amazon; <br /> Research Consultant: Lord
-            Cultural Resources; <br /> Journalist: Times Group and Indian
-            Express Group
-          </Text>
-        </Box>
-      </Responsive>
-    </Box>
-  </Grommet>
-)
-
 const ContributorsPage = () => (
   <AppShell
     headerLabel={"Tattle"}
@@ -178,9 +39,80 @@ const ContributorsPage = () => (
     primaryNav={primaryNav}
     expandCenter={true}
   >
-    <ResponsiveGrid title={"Full Time Staff"} />
-    <ResponsiveGrid title={"Advisors"} />
+    <ResponsiveContributorGrid
+      columns={columns}
+      rows={rows}
+      areas={fixedGridAreas}
+      title="Full Time Staff"
+      data={[
+        {
+          Type: "Full Time",
+          Avatar:
+            "//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80",
+          Name: "Denny George",
+          Title: "Interaction design and tech lead",
+          Past:
+            "Director of Mobile Products on Micragram, and WorkIt at Quadrant2; New York ITP, Tisch School of Arts, NYU",
+        },
+        {
+          Type: "Full Time",
+          Avatar:
+            "//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80",
+          Name: "Tarunima Prabhakar",
+          Title: "Research and Project Lead",
+          Past:
+            "Past: Research Fellow, CLTC, UC, Berkeley; Facebook Policy Research",
+        },
+        {
+          Type: "Full Time",
+          Avatar:
+            "//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80",
+          Name: "Kruttika Nadig",
+          Title: "Data Scientist",
+          Past:
+            "Past: Marketing Manager, Amazon; Research Consultant: Lord Cultural Resources; Journalist: Times Group and Indian Express Group",
+        },
+      ]}
+    />
 
+    <ResponsiveContributorGrid
+      columns={columns}
+      rows={rows}
+      areas={fixedGridAreas}
+      title="Advisors"
+      data={[
+        {
+          Type: "Advisor",
+          Name: "Eishita Chhetri",
+          Role: "Illustrator",
+        },
+        {
+          Type: "Advisor",
+          Name: "Sunandan Chakraborty",
+          Role: "Assistant Professor, Information School, Indiana University",
+        },
+        {
+          Type: "Advisor",
+          Name: "Vrinda Bhandari",
+          Role: "External Legal Counsel for India",
+        },
+        {
+          Type: "Advisor",
+          Name: "Nilomee Jesrani",
+          Role: "UX / Visual Design",
+        },
+        {
+          Type: "Advisor",
+          Name: "Saakshita Prabhakar",
+          Role: "UX / Visual Design",
+        },
+        {
+          Type: "Advisor",
+          Name: "Swair Shah",
+          Role: "Data Scientist",
+        },
+      ]}
+    />
     <Text size={"medium"}>
       <i>
         Several other people have volunteered time and skills such as writing,
