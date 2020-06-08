263c236a84d49a4486647329d12b83573e9bfc0f
denny <denny.george90@gmail.com>
2020-06-08 23:09:18 +0530

    (chore) add gatsby plugin for styled components to address the initial flicker on load for the live website

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,6 +84,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
     // {
     //   resolve: `gatsby-source-ghost`,
     //   options: {
