d372eae569019e622e5b1478dbd062b54745cb94
Denny <denny.george90@gmail.com>
2021-08-03 22:07:18 +0530

    feat : add research page

---
+++ src/config/research.js
@@ -0,0 +1,59 @@
+export const research = {
+  featured: [
+    {
+      date: "30 July, 2021",
+      title:
+        "Crowdsourcing Aid: A case study of the Information Chaos during India's Second Covid-19 Wave",
+      description:
+        "This report contends that the second wave of the pandemic in India showed a new facet of the Information Disorder. It was driven by a specific typology of information—of hyperlocal information leads shared during a crisis.",
+      url: "https://icwsm.org/2021/#accepted_papers",
+      tags: ["Report", "Whatsapp", "Vizualization"],
+    },
+    {
+      date: "10 June, 2021",
+      title:
+        "Check Mate: Prioritizing User Generated Multi-Media Content for Fact-Checking",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare nibh vel orci tristique, pellentesque elementum sapien molestie. Pellentesque interdum enim vel quam dignissim faucibus.",
+      url: "https://icwsm.org/2021/#accepted_papers",
+      tags: ["Paper", "Talk"],
+    },
+  ],
+  all: [
+    {
+      date: "10 June, 2021",
+      title:
+        "A Contextual Inquiry of The International Fact-Checking Network and Factuality on Social Media",
+      description:
+        "Fusce placerat lectus quis magna vulputate placerat. Fusce id augue nisl. ",
+      url: "https://icwsm.org/2021/#accepted_papers",
+      tags: ["Paper", "Talk"],
+    },
+    {
+      date: "10 June, 2021",
+      title:
+        "Against Platform Universalism: A Case Study of Platform-Seeded Trends and Non/Algorithmic Paternalism from India",
+      description:
+        "Donec ac mattis velit. Nulla volutpat velit et dapibus laoreet.",
+      url: "https://icwsm.org/2021/#accepted_papers",
+      tags: ["Paper", "Talk"],
+    },
+    {
+      date: "10 June, 2021",
+      title: "Ethical Approaches to Closed Messaging Research",
+      description:
+        "Sed at risus urna. Mauris ornare sapien quam, nec iaculis est porta a. Integer sed nisl commodo, mollis elit vel, commodo lectus.",
+      url: "https://icwsm.org/2021/#accepted_papers",
+      tags: ["Paper", "Talk"],
+    },
+    {
+      date: "10 June, 2021",
+      title:
+        "Considerations in Archiving Content from Encrypted Messaging Apps",
+      description:
+        "Morbi scelerisque mollis dolor sit amet eleifend. Aliquam mauris enim, efficitur non rhoncus vel, scelerisque eget nibh. ",
+      url: "https://icwsm.org/2021/#accepted_papers",
+      tags: ["Paper", "Talk"],
+    },
+  ],
+}
---
+++ src/pages/research.js
@@ -0,0 +1,108 @@
+import React, { useContext } from "react"
+import NarrowContentWrapper from "../components/atomic/layout/narrow-content-wrapper"
+import NarrowSection from "../components/atomic/layout/narrow-section"
+import DefaultLayout from "../components/default-layout"
+import {
+  Box,
+  Heading,
+  Paragraph,
+  ResponsiveContext,
+  Text,
+  Image,
+} from "grommet"
+import { research } from "../config/research"
+import TagBubble from "../components/atomic/TagBubble"
+import { PlainExternalLink } from "../components/atomic/TattleLinks"
+import { ExternalLink } from "react-feather"
+
+const FeaturedListItem = ({ item }) => {
+  const size = useContext(ResponsiveContext)
+  return (
+    <Box
+      direction={"column"}
+      onClick={() => {}}
+      hoverIndicator={true}
+      width={size === "small" ? "100%" : "50%"}
+    >
+      <PlainExternalLink href={item.url} target={"_blank"}>
+        <Paragraph size={"small"} color={"dark-3"} margin={"none"}>
+          {item.date}
+        </Paragraph>
+        <Box direction={"row-responsive"} justify={"stretch"}>
+          <Box>
+            <Heading level={3} margin={{ bottom: "4.578px", top: "7.324px" }}>
+              {item.title}
+            </Heading>
+            <Box direction={"row-responsive"} gap={"xsmall"}>
+              {item.tags.map(tag => (
+                <TagBubble data={{ label: tag }} />
+              ))}
+            </Box>
+            <Paragraph size={"medium"}>{item.description}</Paragraph>
+          </Box>
+        </Box>
+      </PlainExternalLink>
+    </Box>
+  )
+}
+
+const AllListItem = ({ item }) => (
+  <Box direction={"column"} onClick={() => {}} hoverIndicator={true}>
+    <PlainExternalLink href={item.url} target={"_blank"}>
+      <Paragraph size={"small"} color={"dark-3"} margin={"none"}>
+        {item.date}
+      </Paragraph>
+      <Box direction={"row-responsive"} justify={"stretch"}>
+        <Box>
+          <Heading
+            level={4}
+            margin={{ bottom: "4.578px", top: "7.324px" }}
+            fill
+          >
+            {item.title}
+          </Heading>
+          <Box direction={"row-responsive"} gap={"xsmall"}>
+            {item.tags.map(tag => (
+              <TagBubble data={{ label: tag }} />
+            ))}
+          </Box>
+          <Paragraph size={"medium"} fill>
+            {item.description}
+          </Paragraph>
+        </Box>
+      </Box>
+    </PlainExternalLink>
+    <Box height={"xxsmall"} />
+  </Box>
+)
+
+const Research = () => {
+  return (
+    <DefaultLayout>
+      <NarrowContentWrapper>
+        <NarrowSection>
+          <Box>
+            <Heading level={2}>Research</Heading>
+
+            <NarrowSection>
+              <Box direction={"row-responsive"} gap={"large"}>
+                {research.featured.map(update => (
+                  <FeaturedListItem item={update} />
+                ))}
+              </Box>
+            </NarrowSection>
+            <NarrowSection>
+              <Box direction={"column"}>
+                {research.all.map(update => (
+                  <AllListItem item={update} />
+                ))}
+              </Box>
+            </NarrowSection>
+          </Box>
+        </NarrowSection>
+      </NarrowContentWrapper>
+    </DefaultLayout>
+  )
+}
+
+export default Research
