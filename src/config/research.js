758ee71ec6a3b40431315b08cb65cf85db76cfc8
tarunima <tarunimap@gmail.com>
2021-08-16 16:40:19 +0530

    Update research.js

--- src/config/research.js
+++ src/config/research.js
@@ -5,55 +5,67 @@ export const research = {
       title:
         "Crowdsourcing Aid: A case study of the Information Chaos during India's Second Covid-19 Wave",
       description:
-        "This report contends that the second wave of the pandemic in India showed a new facet of the Information Disorder. It was driven by a specific typology of information—of hyperlocal information leads shared during a crisis.",
-      url: "https://icwsm.org/2021/#accepted_papers",
-      tags: ["Report", "Whatsapp", "Vizualization"],
+        "Based on analysis of Covid-19 relief groups on WhatsApp, this report contends that the second wave in India showed a new facet of Information Disorder that challenged existing conceptions of misinformation response.",
+      url: "https://tattle.co.in/articles/covid-whatsapp-public-groups/",
+      tags: ["Whatsapp", "Covid-19", "India", "Report"],
     },
+
     {
       date: "10 June, 2021",
       title:
         "Check Mate: Prioritizing User Generated Multi-Media Content for Fact-Checking",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare nibh vel orci tristique, pellentesque elementum sapien molestie. Pellentesque interdum enim vel quam dignissim faucibus.",
+        "This paper presents a novel dataset that can be used to prioritize check-worthy posts from multi-media content in Hindi. It is unique in its 1) focus on user generated content, 2) language and 3) accommodation of multi-modality in social media posts. This Dataset was accepted at ICWSM 2021 . 
+        [Arxiv Preprint:](https://arxiv.org/abs/2010.13387)
+",
       url: "https://icwsm.org/2021/#accepted_papers",
-      tags: ["Paper", "Talk"],
+      tags: ["Dataset", "Multi-media", "Claims-detection"],
     },
   ],
   all: [
     {
-      date: "10 June, 2021",
+      date: "7 June, 2021",
       title:
         "A Contextual Inquiry of The International Fact-Checking Network and Factuality on Social Media",
       description:
-        "Fusce placerat lectus quis magna vulputate placerat. Fusce id augue nisl. ",
-      url: "https://icwsm.org/2021/#accepted_papers",
-      tags: ["Paper", "Talk"],
+        "ICWSM 2021 Workshop on Information Credibility & Alternative Realities in Troubled Democracies",
+      url: "https://zivepstein.github.io/info-credibility-workshop/",
+      tags: ["Factuality", "IFCN", "Abstract"],
     },
     {
-      date: "10 June, 2021",
+      date: "24 March, 2021",
       title:
         "Against Platform Universalism: A Case Study of Platform-Seeded Trends and Non/Algorithmic Paternalism from India",
       description:
-        "Donec ac mattis velit. Nulla volutpat velit et dapibus laoreet.",
-      url: "https://icwsm.org/2021/#accepted_papers",
-      tags: ["Paper", "Talk"],
+        "Presentation at the Annual Conference of Platform Governance Research Network",
+      url: "https://www.platgov.net/program.pdf",
+      tags: ["Indian Social Media", "Paper", "Talk"],
     },
     {
-      date: "10 June, 2021",
-      title: "Ethical Approaches to Closed Messaging Research",
+      date: "15 March, 2021",
+      title: "Ethical Approaches to Closed Messaging Research: Considerations in Democratic Contexts",
       description:
-        "Sed at risus urna. Mauris ornare sapien quam, nec iaculis est porta a. Integer sed nisl commodo, mollis elit vel, commodo lectus.",
-      url: "https://icwsm.org/2021/#accepted_papers",
-      tags: ["Paper", "Talk"],
+        "Report on responsible data collection from chat applications.",
+      url: "https://electionstandards.cartercenter.org/verifying-elections-misinfocon2020/ethical-approaches-to-closed-messaging-research-considerations-in-democratic-contexts/",
+      tags: ["Responsible Data Collection", "Chat Apps", "Paper"],
     },
     {
-      date: "10 June, 2021",
+      date: "4 October, 2019",
       title:
         "Considerations in Archiving Content from Encrypted Messaging Apps",
       description:
-        "Morbi scelerisque mollis dolor sit amet eleifend. Aliquam mauris enim, efficitur non rhoncus vel, scelerisque eget nibh. ",
-      url: "https://icwsm.org/2021/#accepted_papers",
-      tags: ["Paper", "Talk"],
+        "Abstract presentation at Workshop on Comparative Approaches to Disinformation, Berkman Klein Center, Harvard University",
+      url: "https://cyber.harvard.edu/story/2019-10/comparative-approaches-disinformation",
+      tags: ["Responsible Data Collection", "Chat Apps", "Paper"],
+    },
+    {
+      date: "4 October, 2019",
+      title:
+        "Data Collection and Management at Tattle",
+      description:
+        "Presentation at Conference for Truth and Trust Online",
+      url: "https://truthandtrustonline.com/2019-proceedings/",
+      tags: ["Tattle Process","Paper"],
     },
   ],
 }
