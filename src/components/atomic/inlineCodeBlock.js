718d65fc0a08ad873fe0b879a87293ebd7ff16e8
Aatman Vaidya <aatmanvaidya@gmail.com>
2023-08-26 00:38:11 +0530

    fixing inline UI

--- src/components/atomic/inlineCodeBlock.js
+++ src/components/atomic/inlineCodeBlock.js
@@ -2,7 +2,13 @@ import React from "react";
 import { dracula } from "react-code-blocks";
 
 const InlineCodeBlock = ({ children }) => (
-    <code style={{ backgroundColor: dracula.backgroundColor, color: dracula.textColor }}>
+    <code style={{ 
+        backgroundColor: "#FFEBCB",
+        color: "#514E80",
+        padding: "2px 6px",
+        borderRadius: "4px",
+        fontFamily: "Menlo, Monaco, Consolas, 'Courier New', monospace",
+    }}>
         {children}
     </code>
 );
