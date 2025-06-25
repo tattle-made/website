80a0d2d4b52ac67fd866ef3c26816e2427b679ff
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-06-25 08:51:45 +0530

    docs: add JSDoc for BlogHeadercard component

--- src/components/atomic/BlogHeaderCard.js
+++ src/components/atomic/BlogHeaderCard.js
@@ -2,6 +2,17 @@ import React from "react"
 import { Box, Heading, Text } from "grommet"
 import { byline } from "../default-blog-index-layout"
 
+/**
+ * Renders the header section of a blog post card with title, author, date, and project info.
+ *
+ * @param {Object} props
+ * @param {string} props.name - Blog post title.
+ * @param {string} props.author - Author name.
+ * @param {string} props.date - Publication date string.
+ * @param {string} [props.project] - Optional project associated with the blog.
+ * @returns {JSX.Element} Blog header card component.
+ */
+
 const BlogHeaderCard = ({ name, author, date, project }) => (
   <Box margin={{ bottom: "small" }}>
     <Box direction={"row"} align={"center"}>
