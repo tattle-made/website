585e2fbe5b52e060aa04ded1033ec2b21ce5e932
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2025-08-08 16:01:43 +0530

    Remove unused style page from TagsRenderer component

--- src/components/TagsRenderer.jsx
+++ src/components/TagsRenderer.jsx
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import { Box, Heading, Button } from "grommet"
 
 import TagBubbleBlog from "./atomic/TagBubbleBlog"
+// import './styles/global.css';
 
 /**
  * Renders a list of tags with an option to toggle between showing all tags or a limited number.
@@ -29,53 +30,54 @@ export default function TagsRenderer({
 
   return (
     <Box margin={{ bottom: "small" }}>
-    <Box flex direction="row" align="baseline" gap="xsmall">
-      <Box>
-        <Heading level={4} margin={{ bottom: "small" }}>
-          {tagTypeHeading}
-        </Heading>
-      </Box>
+      <Box flex direction="row" align="baseline" gap="xsmall">
+        <Box>
+          <Heading level={4} margin={{ bottom: "small" }}>
+            {tagTypeHeading}
+          </Heading>
+        </Box>
 
-      <Box flex direction="row" gap="small" align="center" wrap={true}>
-        {showAllTags
-          ? sortedUniqueTags.map((tag) => (
-            <Box key={tag} margin={{ bottom: "small" }}>
-              <Link
-                to={tagBaseURL.concat(tag)}
-                style={{ textDecoration: "none" }}
+        <Box flex direction="row" gap="small" align="center" wrap={true}>
+          {showAllTags
+            ? sortedUniqueTags.map((tag) => (
+              <Box key={tag} margin={{ bottom: "small" }}>
+                <Link
+                  to={tagBaseURL.concat(tag)}
+                  style={{ textDecoration: "none" }}
+                >
+                  <TagBubbleBlog data={{ label: tag, count: tagCounts[tag] }} />
+                </Link>
+              </Box>
+            ))
+            : sortedUniqueTags.slice(0, 10).map((tag) => (
+              <Box key={tag}
+                pad={{ vertical: "xsmall" }}
+                round="xsmall"
+                margin={{ bottom: "xxsmall" }}>
+                <Link
+                  to={tagBaseURL.concat(tag)}
+                  style={{ textDecoration: "none" }}
+                >
+                  <TagBubbleBlog data={{ label: tag, count: tagCounts[tag] }} />
+                </Link>
+              </Box>
+            ))}
+          {sortedUniqueTags.length > 10 && (
+            <Button onClick={toggleTagsDisplay}>
+              <Box
+                pad={{ horizontal: "small", vertical: "xsmall" }}
+                align="center"
+                border={{ color: "#E76D67", size: "1px" }}
+                round="small"
+                className="hover:ring-2 hover:ring-E76D67"
               >
-                <TagBubbleBlog data={{ label: tag, count: tagCounts[tag] }} />
-              </Link>
-            </Box>
-          ))
-          : sortedUniqueTags.slice(0, 10).map((tag) => (
-            <Box key={tag}
-              pad={{ vertical: "xsmall" }}
-              round="xsmall"
-              margin={{  bottom: "xxsmall" }}>
-              <Link
-                to={tagBaseURL.concat(tag)}
-                style={{ textDecoration: "none" }}
-              >
-                <TagBubbleBlog data={{ label: tag, count: tagCounts[tag] }} />
-              </Link>
-            </Box>
-          ))}
-        {sortedUniqueTags.length > 10 && (
-          <Button onClick={toggleTagsDisplay}>
-            <Box
-              pad={{ horizontal: "small", vertical: "xsmall" }}
-              align="center"
-              border={{ color: "#E76D67", size: "1px" }}
-              round="small"
-              className="hover:ring-2 hover:ring-E76D67"
-            >
-              {showAllTags ? "Show Less Tags" : "Show All Tags"}
-            </Box>
-          </Button>
-        )}
-      </Box>
+                {showAllTags ? "Show Less Tags" : "Show All Tags"}
+              </Box>
+            </Button>
+          )}
+        </Box>
       </Box>
     </Box>
   )
 }
+
