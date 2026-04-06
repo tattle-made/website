a303a9c95f99c55c269bb30a241794bc79cb7a22
Manisha Kumari <manisha.tkg08@gmail.com>
2026-04-06 20:58:47 +0530

    Make author names clickable and fix community page styles

--- src/components/atomic/BlogHeaderCard.js
+++ src/components/atomic/BlogHeaderCard.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Box, Heading, Text } from "grommet"
-import { byline } from "../default-blog-index-layout"
-import { Link } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { PlainLink } from "./TattleLinks"
 
 /**
  * Renders the header section of a blog post with title, authors (with avatars), date, and project.
@@ -15,35 +15,58 @@ import { Link } from "gatsby"
  * @returns {JSX.Element} Blog header card component.
  */
 
-const BlogHeaderCard = ({ name, author, date, project, textColor, authorSlug }) => {
+const AuthorAvatar = ({ author, textColor }) => {
+  const img = author?.frontmatter?.img ? getImage(author.frontmatter.img) : null
+  const name = author?.frontmatter?.name
+  const slug = author?.fields?.slug
 
-  return (
-    <Box margin={{ bottom: "small" }}>
-      <Box direction={"row"} align={"center"}>
-        <Heading level={2} margin={"none"} weight={500} fill={true} color={textColor}>
-          {name}
-        </Heading>
+  const inner = (
+    <Box direction="row" align="center" gap="xsmall">
+      <Box
+        width="32px"
+        height="32px"
+        round="full"
+        overflow="hidden"
+        flex={{ shrink: 0 }}
+        background="light-3"
+      >
+        {img && (
+          <GatsbyImage
+            alt={`${name}'s photo`}
+            image={img}
+            style={{ width: "100%", height: "100%" }}
+          />
+        )}
       </Box>
-
-      <Text size={"small"} color={textColor}>
-        {`Published on ${new Date(date).toDateString()}`}
-      </Text>
-
-      <Text size={"small"} color={textColor}>
-        {author?.map((a, index) => (
-          <span key={a.fields.slug}>
-            <Link
-              to={`/people/${a.fields.slug}`}
-              style={{ textDecoration: "none", color: textColor }}
-            >
-              {a.frontmatter.name}
-            </Link>
-            {index < author.length - 1 ? ", " : ""}
-          </span>
-        ))}
+      <Text size="small" color={ textColor || "inherit" || "dark-1"} weight={500}>
+        {name}
       </Text>
     </Box>
   )
+
+  return slug ? (
+    <PlainLink to={`/people/${slug}`}>{inner}</PlainLink>
+  ) : (
+    inner
+  )
 }
+
+const BlogHeaderCard = ({ name, authors = [], date, project, textColor }) => (
+  <Box margin={{ bottom: "small" }}>
+    <Heading level={2} margin={"none"} weight={500} fill={true} color={textColor}>
+      {name}
+    </Heading>
+    <Text size={"small"} color={textColor}>{`Published on ${new Date(
+      date
+    ).toDateString()}`}</Text>
+    {authors?.length > 0 && (
+      <Box direction="row" gap="medium" wrap margin={{ top: "xsmall" }}>
+        {authors.map((a) => (
+          <AuthorAvatar key={a?.fields?.slug || a?.frontmatter?.name} author={a} textColor={textColor} />
+        ))}
+      </Box>
+    )}
+  </Box>
+)
 
 export default BlogHeaderCard
