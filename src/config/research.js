928b2a37751b9dfd7ab299693fd3758663bd7d40
Manisha Kumari <manisha.tkg08@gmail.com>
2026-09-11 14:08:07 +0530

    Update research entries order

--- src/config/research.js
+++ src/config/research.js
@@ -27,9 +27,16 @@ export const research = {
         "online gender-based violence",
       ],
     },
-    
+
   ],
   all: [
+    {
+      date: "3 june 2026",
+      title: "Desi Faces, Foreign Servers: Inside the AI Economy Undressing Women",
+      description: "A journalistic collaboration to understand the infrastructures that enable production of non-consensual synthetic imagery.",
+      url: "https://www.decodeinternet.in/life/investigation/desi-faces-foreign-servers-inside-the-ai-economy-undressing-women-31691",
+      tags: ["Article", "Online Gender-based Violence", "NCII"],
+    },
     {
       date: "30th May, 2026",
       title: "Quantifying the Illicit Ecosystem of Betting Apps in India",
@@ -37,6 +44,13 @@ export const research = {
         "Quantifies India’s online betting apps ecosystem by linking social media promotion to user harm",
       url: "https://ojs.aaai.org/index.php/ICWSM/article/view/42752",
       tags: ["dataset", "betting apps", "financial harm"],
+    },
+    {
+      date: "18 Sep 2025",
+      title: "Participation in AI: Notes from the trenches",
+      description: "Book Chapter reflecting on building an AI model for detection of gendered abuse in India by centering and working with those affected by gendered abuse, as a part of Uli.",
+      url: "https://library.oapen.org/bitstream/handle/20.500.12657/107728/1/9781040427989.pdf#page=87",
+      tags: ["Book Chapter", "Gender Abuse", "ML"],
     },
     {
       date: "8th December, 2023",
@@ -156,19 +170,5 @@ export const research = {
       url: "https://truthandtrustonline.com/2019-proceedings/",
       tags: ["Tattle Process", "Paper"],
     },
-    {
-      date: "18 Sep 2025",
-      title: "Participation in AI: Notes from the trenches",
-      description: "Book Chapter reflecting on building an AI model for detection of gendered abuse in India by centering and working with those affected by gendered abuse, as a part of Uli.",
-      url: "https://library.oapen.org/bitstream/handle/20.500.12657/107728/1/9781040427989.pdf#page=87",
-      tags: ["Book Chapter", "Gender Abuse", "ML"],
-    },
-    {
-      date: "3 june 2026",
-      title: "Desi Faces, Foreign Servers: Inside the AI Economy Undressing Women",
-      description: "A journalistic collaboration to understand the infrastructures that enable production of non-consensual synthetic imagery.",
-      url: "https://www.decodeinternet.in/life/investigation/desi-faces-foreign-servers-inside-the-ai-economy-undressing-women-31691",
-      tags: ["Article", "Online Gender-based Violence", "NCII"],
-    }
-  ],
+  ]
 }
